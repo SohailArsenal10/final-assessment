5631bb05d34cbe110686eab6172ed988aa3a43b2
Syed Sohail Ahmed <sohahm1999@gmail.com>
2020-12-19 00:43:35 +0530

    Update app-routing.module.ts

--- berkadiafinalassessment/src/app/app-routing.module.ts
+++ berkadiafinalassessment/src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { updatecustomersComponent } from './components/updatecustomers/updatecus
 
 
 const routes: Routes = [
+  {path : "" , redirectTo : /getcustomerdetails , prefix : full },
   {path: "getcustomerdetails", component: CustomerlistComponent},
-  {path: "insertcustomerdetails", component: updatecustomersComponent}
-
-];
+  {path: "insertcustomerdetails", component: updatecustomersComponent},
+  {path : "**" , component : errorComponent },                                        //Order important as ** can mean any of the above in regular expression but is generally
+  
+ ];                                                                                         //meant for other than above
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
